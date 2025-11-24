3276cce3e86d138e7f83bf0c6697233b4266a2b8
Guy Modai <Guy.modai@post.runi.ac.il>
2025-11-25 00:28:02 +0200

    test02

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -153,11 +153,11 @@ public class ArrCharOps {
      * lexicographically greater than str2.
      * return -2 if there is an error with the input.
      */
-    public static int compareTo(String str1, String str2) { 
-
-        if (str1 == null || str2 == null) {
+   public static int compareTo(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
             return -2;
         }
+
         int minLength = Math.min(str1.length(), str2.length());
 
         for (int i = 0; i < minLength; i++) {
